9ea26da9917723d1895c2fd6befa9dd1a7e0b9cd
Erick Fuentes <fuentes.erick@gmail.com>
2014-09-15 16:00:18 -0400

    Test Code for printing

--- app/main.c
+++ app/main.c
@@ -3,6 +3,7 @@
 
 #include "lld.h"
 #include "gde021a1.h"
+#include "printf.h"
 
 
 #define RED_LED GPIOA, 5
@@ -11,6 +12,9 @@
 #define COLOR_SCL GPIOB, 6
 #define COLOR_SDA GPIOB, 7
 #define COLOR_INT GPIOB, 12
+
+#define USART_TX GPIOA, 9
+#define USART_RX GPIOA, 10
 
 static volatile uint32_t delayTime = 0;
 volatile uint32_t ticks = 0;
@@ -34,6 +38,10 @@ void delay(uint32_t delayMillis) {
     while(delayTime > 0);
 }
 
+void printfPutC(void *p, char c) {
+    usartPutChar((USART_TypeDef *)p, (uint8_t)c);
+}
+
 int main() {
     if (SysTick_Config(SystemCoreClock/1000)) {
         while(1);
@@ -44,6 +52,14 @@ int main() {
     gpioPinMode(GREEN_LED, gpioMode_output);
     gpioPinMode(RED_LED, gpioMode_output);
 
+    gpioPinMode(USART_RX, gpioMode_alternate);
+    gpioPinMode(USART_TX, gpioMode_alternate);
+    gpioAlternate(USART_RX, 4);
+    gpioAlternate(USART_TX, 4);
+
+
+    usartInit(USART1, 115200);
+    init_printf((void *)USART1, printfPutC);
     gde021a1Init();
 
     gde021a1Test();
@@ -52,6 +68,7 @@ int main() {
     while(1){
         delay(500);
         gpioTogglePin(GREEN_LED);
+        printf("Hello World!\r\n");
     }
 
     return 0;
